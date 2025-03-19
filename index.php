3470f09032d5387e9ca8acd4b0a1af851fed9dc4
thienpr <dotuanthiendz112@gmail.com>
2025-03-19 19:53:42 +0700

    update gd

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// if (isset($_SESSION['Message'])) {
-//     $successMessage = $_SESSION['Message'];
-//     unset($_SESSION['Message']);
-//     echo "<script>alert('$successMessage');</script>";
-// }
